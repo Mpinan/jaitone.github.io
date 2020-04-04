fbcafe73229e2e9e09d1da545c9617c742824e61
Mario90 <mgpinan1990@gmail.com>
2020-04-04 17:38:42 +0100

    changed titles

--- mario/src/Components/modals.jsx
+++ mario/src/Components/modals.jsx
@@ -16,28 +16,22 @@ const PopUpModal = props => {
         Team work and Adaptability.
       </Button>
       <Modal isOpen={modal} toggle={toggle}>
-        <ModalHeader toggle={toggle}>Resilience</ModalHeader>
+        <ModalHeader toggle={toggle}>Team work and Adaptability.</ModalHeader>
         <ModalBody>
           - I am driven person with a can-do attitude which allows me to perform
           tasks over several hours while remaining my focused.
           <br />- I consider myself as a positive person that always try to look
           at the bright side even in I am in the middle of a "storm".
         </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={toggle}>
-            Do Something
-          </Button>{" "}
-          <Button color="secondary" onClick={toggle}>
-            Cancel
-          </Button>
-        </ModalFooter>
       </Modal>
 
       <Button color="danger" onClick={toggle2} style={{ margin: "10px" }}>
         Managing and Work under pressure
       </Button>
       <Modal isOpen={modal2} toggle={toggle2}>
-        <ModalHeader toggle={toggle2}>Resilience</ModalHeader>
+        <ModalHeader toggle={toggle2}>
+          Managing and Work under pressure
+        </ModalHeader>
         <ModalBody>
           - During my work as a chef, large groups of people were served in 3
           hours.
@@ -47,20 +41,14 @@ const PopUpModal = props => {
           <br />- Meeting tight deadlines have been developed. During lunchtime,
           apart from hundreds of people, takeaway orders were taken too.
         </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={toggle2}>
-            Do Something
-          </Button>{" "}
-          <Button color="secondary" onClick={toggle2}>
-            Cancel
-          </Button>
-        </ModalFooter>
       </Modal>
       <Button color="danger" onClick={toggle3} style={{ margin: "10px" }}>
         Resilient with a Can-do attitude
       </Button>
       <Modal isOpen={modal3} toggle={toggle3}>
-        <ModalHeader toggle={toggle3}>Resilience</ModalHeader>
+        <ModalHeader toggle={toggle3}>
+          Resilient with a Can-do attitude
+        </ModalHeader>
         <ModalBody>
           - In Makers, we were put with different pairs every week as well as in
           the group projects.
@@ -68,14 +56,6 @@ const PopUpModal = props => {
           more ideas from it and hear different opinions as well as getting good
           feedback. I am also able to adapt to every person and its needs.
         </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={toggle3}>
-            Do Something
-          </Button>{" "}
-          <Button color="secondary" onClick={toggle3}>
-            Cancel
-          </Button>
-        </ModalFooter>
       </Modal>
     </div>
   );
