04a838ab542a5f06d0ee19a8a7cf2a8a358aded8
Mario90 <mgpinan1990@gmail.com>
2020-04-12 19:10:16 +0100

    overlay

--- mario/src/Components/projects.jsx
+++ mario/src/Components/projects.jsx
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
-import img1 from "../Assets/img.png";
-import img2 from "../Assets/weatherapp.jpg";
-import img3 from "../Assets/Acebookapp.png";
-import img4 from "../Assets/me.jpg";
+import img1 from "../Assets/1_MCn1VrXQznTi3JMtAIGK0g.jpeg";
 import {
   Container,
   Row,
@@ -16,42 +13,72 @@ import {
 class Projects extends Component {
   render() {
     return (
-      <Container style={{ textAlign: "center" }}>
-        <div className="img">
-          <div style={{ marginTop: "40px", marginBottom: "40px" }}>
-            <img src={img2} alt="Avatar" class="image" />
-            <div style={{ marginTop: "40px" }} class="overlay">
-              <div class="text">Hello World</div>
+      <Container>
+        <h1>Some of my work</h1>
+        <div className="row">
+          <div className="col">
+            <div className="img">
+              <div style={{ marginTop: "40px", marginBottom: "40px" }}>
+                <img src={img1} alt="Avatar" class="image" />
+                <div style={{ marginTop: "40px" }} class="overlay">
+                  <div class="text">Hello World</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="img">
+              <div style={{ marginTop: "40px", marginBottom: "40px" }}>
+                <img src={img1} alt="Avatar" class="image" />
+                <div style={{ marginTop: "40px" }} class="overlay">
+                  <div class="text">Hello World</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="img">
+              <div style={{ marginTop: "40px", marginBottom: "40px" }}>
+                <img src={img1} alt="Avatar" class="image" />
+                <div style={{ marginTop: "40px" }} class="overlay">
+                  <div class="text">Hello World</div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="img">
-          <div style={{ marginTop: "40px" }}>
-            <img src={img1} alt="Avatar" class="image" />
-            <div
-              style={{ marginTop: "40px", marginBottom: "40px" }}
-              class="overlay"
-            >
-              <div class="text">World</div>
+        {/* <div className="row">
+          <div className="col">
+            <div className="img">
+              <div style={{ marginTop: "40px", marginBottom: "40px" }}>
+                <img src={img1} alt="Avatar" class="image" />
+                <div style={{ marginTop: "40px" }} class="overlay">
+                  <div class="text">Hello World</div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="img">
-          <div style={{ marginTop: "40px", marginBottom: "40px" }}>
-            <img src={img3} alt="Avatar" class="image" />
-            <div style={{ marginTop: "40px" }} class="overlay">
-              <div class="text">World</div>
+          <div className="col">
+            <div className="img">
+              <div style={{ marginTop: "40px", marginBottom: "40px" }}>
+                <img src={img1} alt="Avatar" class="image" />
+                <div style={{ marginTop: "40px" }} class="overlay">
+                  <div class="text">Hello World</div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="img">
-          <div style={{ marginTop: "40px", marginBottom: "40px" }}>
-            <img src={img4} alt="Avatar" class="image" />
-            <div style={{ marginTop: "40px" }} class="overlay">
-              <div class="text">World</div>
+          <div className="col">
+            <div className="img">
+              <div style={{ marginTop: "40px", marginBottom: "40px" }}>
+                <img src={img1} alt="Avatar" class="image" />
+                <div style={{ marginTop: "40px" }} class="overlay">
+                  <div class="text">Hello World</div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </Container>
     );
   }
